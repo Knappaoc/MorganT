edc2a6085aabfac2368613a88f5949c3a666ef68
Morgan Trench <morgan.trench@gmail.com>
2022-12-04 21:37:49 +0800

    day 4 improvements

--- src/main/kotlin/day_4/day_4.kt
+++ src/main/kotlin/day_4/day_4.kt
@@ -1,27 +1,28 @@
 package day_4
 
+import measureMedians
 import java.io.File
 
 fun main() {
     println("Answer 1: ${stepOne()}")
     println("Answer 2: ${stepTwo()}")
-//    println("median durations were ${measureMedians(listOf(::stepOne, ::stepTwo)).joinToString(" and ")} respectively")
+    println("median durations were ${measureMedians(listOf(::stepOne, ::stepTwo), 30000).joinToString(" and ")} respectively")
 }
 
 fun String.toRange() = this.split("-").let { it[0].toInt() to it[1].toInt() }.run { first..second }
 
+infix fun IntRange.covers(target: IntRange) = first <= target.first && target.last <= last
+
 fun stepOne(): Int =
     File("data/day_4.txt").useLines { lines ->
         val pairs = lines.map{ line -> line.split(",").let { it[0].toRange() to it[1].toRange() } }
-        pairs.count { (first, second) ->
-            first.all { second.contains(it) } || second.all { first.contains(it) }
-        }
+        pairs.count { (first, second) -> first covers second || second covers first }
     }
+
+infix fun IntRange.overlaps(target: IntRange) = contains(target.first) || contains(target.last) || (target covers this)
 
 fun stepTwo(): Int =
     File("data/day_4.txt").useLines { lines ->
         val pairs = lines.map{ line -> line.split(",").let { it[0].toRange() to it[1].toRange() } }
-        pairs.count { (first, second) ->
-            first.any { second.contains(it) } || second.any { first.contains(it) }
-        }
+        pairs.count { (first, second) -> first overlaps second }
     }
