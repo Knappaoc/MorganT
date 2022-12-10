0b9b02338fd68a5fa1dbe83e8bd874fd35bd64b9
Morgan Trench <morgan.trench@gmail.com>
2022-12-11 01:38:52 +0800

    day 10 improvements (still needs more optimizations)

--- src/main/kotlin/day_10/day_10.kt
+++ src/main/kotlin/day_10/day_10.kt
@@ -1,70 +1,58 @@
 package day_10
 
+import measureFastestExecutionTimes
 import java.io.File
 
 fun main() {
-//    println("Answer 1: ${stepOne()}")
-    println("Answer 2: ${stepTwo()}")
-//    println("Fastest execution times ${measureFastestExecutionTimes(listOf(::stepOne, ::stepTwo)).joinToString(" and ")} respectively")
+    println("Answer 1: ${stepOne()}") // 15120
+    println("Answer 2:\n${stepTwo()}") // RKPJBPLA
+    println("Fastest execution times ${measureFastestExecutionTimes(listOf(::stepOne, ::stepTwo)).joinToString(" and ")} respectively")
 }
 
-data class State(val cycle: Int, val x: Int, val dx: Int = 0)
+sealed interface Instruction {
+    val cycles: Int
+}
+
+class Noop : Instruction {
+    override val cycles: Int = 1
+}
+
+data class AddX(val arg: Int) : Instruction {
+    override val cycles: Int = 2
+}
+
+data class StateX(val cycle: Int, val x: Int) {
+    fun apply(instruction: Instruction) =
+        when (instruction) {
+            is Noop -> this.copy(cycle = cycle + instruction.cycles)
+            is AddX -> this.copy(cycle = cycle + instruction.cycles, x = x + instruction.arg)
+        }
+}
+
+fun loadProgram(path: String) = File(path).readLines().map { line ->
+    val tokens = line.split(" ");
+    when (tokens.first()) {
+        "noop" -> Noop();
+        "addx" -> AddX(tokens.last().toInt())
+        else -> throw Error("Problem parsing program line \"$line\"")
+    }
+}
 
 fun stepOne(): Int {
-    val instructions = File("data/day_10.txt").readLines()
-
-    val evolution = instructions.fold(mutableListOf<State>(State(0, 1))) { acc, s ->
-        val settled = acc.last().let { it.copy(x = it.x + it.dx, dx = 0) }
-        if (s == "noop"){
-            acc.add(settled.copy(cycle = settled.cycle + 1))
-        } else {
-            val arg = s.split(" ").last().toInt()
-            acc.addAll(listOf(
-                settled.copy(cycle = settled.cycle + 1),
-                settled.copy(cycle = settled.cycle + 2, dx= arg))
-            )
-        }
-        acc
-    }
-
-    val signalStrengths = (20..220 step 40).map {cycle ->
-        val (_, x) = evolution.find { it.cycle == cycle }!!;
-        cycle * x
-    }
-
+    val program = loadProgram("data/day_10.txt")
+    val evolution = program.scan(StateX(0, 1)) { acc, instruction -> acc.apply(instruction) }
+    val signalStrengths = (20..220 step 40).map { cycle -> evolution.last { state -> state.cycle < cycle }.x * cycle }
     return signalStrengths.sum();
 }
 
-fun stepTwo() {
-    val instructions = File("data/day_10.txt").readLines()
-
-    val evolution = instructions.fold(mutableListOf<State>(State(0, 1))) { acc, s ->
-        val settled = acc.last().let { it.copy(x = it.x + it.dx, dx = 0) }
-        if (s == "noop"){
-            acc.add(settled.copy(cycle = settled.cycle + 1))
-        } else {
-            val arg = s.split(" ").last().toInt()
-            acc.addAll(listOf(
-                settled.copy(cycle = settled.cycle + 1),
-                settled.copy(cycle = settled.cycle + 2, dx= arg))
-            )
-        }
-        acc
-    }
-
-    val height = 6; val width = 40;
-    val screen = List(height){ List(width){ "." }.toMutableList() }
-    for (state in evolution.drop(1)){
-        val (cycle, x) = state;
-        val (px, py) = (cycle - 1) % 40 to (cycle - 1) / 40
-        if (x in listOf(px - 1, px, px + 1)){
-            screen[py][px] = "#";
-        }
-    }
-    println(screen.joinToString("\n") { it.joinToString("") })
+fun stepTwo(): String {
+    val program = loadProgram("data/day_10.txt")
+    val evolution = program.scan(StateX(0, 1)) { acc, instruction -> acc.apply(instruction) }
+    val height = 6; val width = 40
+    val screen = (1..(width * height)).joinToString("") { cycle ->
+        val x = evolution.last { state -> state.cycle < cycle }.x;
+        val px = (cycle - 1) % width
+        if (x in (px - 1)..(px + 1)) "#" else "."
+    }.chunked(width).joinToString("\n")
+    return screen;
 }
-
-//fun stepTwo(): Int {
-//    val input = File("data/test.txt").readLines()
-//    return 1;
-//}
