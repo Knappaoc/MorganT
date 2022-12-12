44396cef152d08b8ed5cce4c878e374f94fdc163
Morgan Trench <morgan.trench@gmail.com>
2022-12-13 01:25:13 +0800

    day 12 tidy

--- src/main/kotlin/day_12/day_12.kt
+++ src/main/kotlin/day_12/day_12.kt
@@ -5,9 +5,9 @@ import java.io.File
 import java.util.PriorityQueue
 
 fun main() {
-//    println("Answer 1: ${stepOne()}")
+    println("Answer 1: ${stepOne()}")
     println("Answer 2: ${stepTwo()}")
-//    println("Fastest execution times ${measureFastestExecutionTimes(listOf(::stepOne, ::stepTwo)).joinToString(" and ")} respectively")
+    println("Fastest execution times ${measureFastestExecutionTimes(listOf(::stepOne, ::stepTwo)).joinToString(" and ")} respectively")
 }
 
 fun Char.height() : Int{
@@ -16,77 +16,64 @@ fun Char.height() : Int{
     return this - 'a';
 }
 
-fun Pair<Int, Int>.bound(xBound: Int, yBound: Int) = if (first in 0 until xBound && second in 0 until yBound) this else null;
+fun Pair<Int, Int>.bound(xBound: Int, yBound: Int) = if (first in 0 until xBound && second in 0 until yBound) this else null
+
+fun shortestPath(start: Pair<Int, Int>, end: Pair<Int, Int>, grid: List<List<Char>>): List<Pair<Int, Int>>? {
+    val width = grid.first().size; val height = grid.size
+
+    val queue = PriorityQueue<List<Pair<Int, Int>>> { a, b -> a.size - b.size };
+    queue.add(listOf(start))
+
+    val visited = mutableSetOf<Pair<Int, Int>>()
+
+    while(queue.isNotEmpty()){
+        val candidate = queue.poll()
+        val position = candidate.last()
+
+        if (position in visited) continue;
+        visited.add(position);
+
+        if (position == end) return candidate
+
+        val (px, py) = position;
+        val neighbours = listOf(px - 1 to py, px + 1 to py, px to py - 1, px to py + 1)
+            .mapNotNull { it.bound(width, height) }
+            .filter { !visited.contains(it) }
+            .filter { (x, y) -> grid[y][x].height() - grid[py][px].height() <= 1 }
+
+        queue.addAll(neighbours.map { candidate + it })
+    }
+
+    return null;
+}
+
+fun loadData(path: String, charactersOfInterest: Set<Char>) = File(path).readLines().let { lines ->
+    val grid = lines.map { line -> line.toList() }
+    val pointOfInterest = grid
+        .asSequence()
+        .withIndex()
+        .flatMap { (y, row) -> row
+            .toList()
+            .withIndex()
+            .map { (x, char) -> char to (x to y) }
+        }
+        .filter { it.first in charactersOfInterest }
+        .groupBy ({ it.first }){ it.second }
+     grid to pointOfInterest;
+}
 
 fun stepOne(): Int {
-    val grid = File("data/day_12.txt").readLines().map { line -> line.toList() }
-    val width = grid[0].size;  val height = grid.size;
-    val startY = grid.indexOfFirst { it.contains('S') }; val startX = grid[startY].indexOf('S');
-    val endY = grid.indexOfFirst { it.contains('E') }; val endX = grid[endY].indexOf('E');
-
-    val start = startX to startY;
-    val end = endX to endY;
-
-    val queue = PriorityQueue<List<Pair<Int, Int>>>() { a, b -> a.size - b.size};
-    queue.add(listOf(start))
-    val visited = mutableSetOf<Pair<Int, Int>>()
-    while(queue.isNotEmpty()){
-        val candidate = queue.poll()
-        val position = candidate.last()
-
-        if (position in visited) continue;
-
-        if (position == end) return candidate.size - 1
-
-        val (px, py) = position;
-        visited.add(position);
-        val neighbours = listOf(px - 1 to py, px + 1 to py, px to py - 1, px to py + 1)
-            .mapNotNull { it.bound(width, height) }
-            .filter { !visited.contains(it) }
-            .filter { (x, y) -> grid[y][x].height() - grid[py][px].height() <= 1 }
-        queue.addAll(neighbours.map { candidate + it })
-    }
-    throw Error("Exhausted");
+    val (grid, pointsOfInterest) = loadData("data/day_12.txt", "SE".toSet());
+    val start = pointsOfInterest['S']!!.single()
+    val end = pointsOfInterest['E']!!.single()
+    val path = shortestPath(start, end, grid)!!
+    return path.size - 1
 }
-
-fun shortestPathLength(start: Pair<Int, Int>, end: Pair<Int, Int>, grid: List<List<Char>>): Int{
-    val width = grid[0].size;  val height = grid.size;
-    val queue = PriorityQueue<List<Pair<Int, Int>>>() { a, b -> a.size - b.size};
-    queue.add(listOf(start))
-    val visited = mutableSetOf<Pair<Int, Int>>()
-    while(queue.isNotEmpty()){
-        val candidate = queue.poll()
-        val position = candidate.last()
-
-        if (position in visited) continue;
-
-        if (position == end) return candidate.size - 1
-
-        val (px, py) = position;
-        visited.add(position);
-        val neighbours = listOf(px - 1 to py, px + 1 to py, px to py - 1, px to py + 1)
-            .mapNotNull { it.bound(width, height) }
-            .filter { !visited.contains(it) }
-            .filter { (x, y) -> grid[y][x].height() - grid[py][px].height() <= 1 }
-        queue.addAll(neighbours.map { candidate + it })
-    }
-    throw Error("Exhausted");
-}
-
 
 fun stepTwo(): Int {
-    val grid = File("data/day_12.txt").readLines().map { line -> line.toList() }
-    val starts = grid.mapIndexed { y, chars -> chars.mapIndexed{ x, char -> if (char.height() == 0) x to y else null} }.flatten().filterNotNull()
-    val endY = grid.indexOfFirst { it.contains('E') }; val endX = grid[endY].indexOf('E');
-    val end = endX to endY;
-
-    val steps = starts.map { start -> try { shortestPathLength(start, end, grid) } catch (e: Error) { null} }
-
-    return steps.filterNotNull().min();
+    val (grid, pointsOfInterest) = loadData("data/day_12.txt", "SaE".toSet());
+    val starts = pointsOfInterest['S']!! + pointsOfInterest['a']!!
+    val end = pointsOfInterest['E']!!.single()
+    val steps = starts.map { start -> shortestPath(start, end, grid) }
+    return steps.filterNotNull().minOf { it.size } - 1;
 }
-
-//fun stepTwo(): Int {
-//    val signal = File("data/day_6.txt").readText().asSequence()
-//    val first = signal.windowed(14).indexOfFirst { it.toSet().size == 14 }
-//    return first + 14;
-//}
